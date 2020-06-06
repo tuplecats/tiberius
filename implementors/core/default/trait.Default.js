250b8046cca89d291a86b49ccba214ebd093b713
Buildkite agent <hello@prisma.io>
2020-06-06 13:41:49 +0000

    [skip ci] Rebuild pages at e88c2f0d6caa47c1dbdac810ed035325bf25f01f

--- implementors/core/default/trait.Default.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["tiberius"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"tiberius/struct.ClientBuilder.html\" title=\"struct tiberius::ClientBuilder\">ClientBuilder</a>","synthetic":false,"types":["tiberius::client::builder::ClientBuilder"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
